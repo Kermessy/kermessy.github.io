8b65c30f9be3e7c8fd04283f840d28eb0e7516b0
Kermes <kermessy@gmail.com>
2024-11-30 23:51:43 +0100

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,9 @@
-// game start scripts
-
 function startGame()
 {
     document.getElementById("cover").remove();
+    document.documentElement.requestFullscreen();
     updateStartScene();
 }
-
-// Scene scripts
 
 var i_scene = 0;
 
@@ -14,8 +11,6 @@ var i_line;
 var i_bg;
 var i_music;
 
-// this is the function for one scene, currently, the only scene. it's trivial to implement more scenes,
-// each of them setting i_line, i_bg, etc. then calling the child functions 'updateLine()', updateBackground(), etc.
 function updateStartScene()
 {
     const scene_max = 200;
@@ -103,26 +98,13 @@ function updateStartScene()
         case 18:
             i_line = i_line + 1;
             break;
-        case 18:
-            i_line = i_line + 1;
-            break;
-
         case 19:
-            i_line = i_line + 1;
-            i_bg = 4;
-            break;
         case 20:
-            i_line = i_line + 1;
-            i_bg = 2;
-            break;
         case 21:
-            i_line = i_line + 1;
-            break;
         case 22:
-            i_line = i_line + 1;
-            break;
         case 23:
             i_line = i_line + 1;
+            i_bg = 4;
             break;
         case 24:
             i_line = i_line + 1;
@@ -450,7 +432,7 @@ function updateStartScene()
             i_line = i_line + 1;
             i_bg = 1;
             i_music = 1;
-            setTimeout(updateStartScene, 10000);
+            setTimeout(updateStartScene, 1000000);
             break;
 
         case 107:
@@ -530,10 +512,10 @@ function updateStartScene()
             console.log("updateStartScene(): current i_scene unimplemented");
             break;
     }
-
-    updateLine();
     updateBackground();
     updateMusic();
+    if (Intl.DateTimeFormat().resolvedOptions().timeZone == 'Europe/Paris') {updateLinefr();}
+    else updateLine();
 }
 
 // line scripts
@@ -591,8 +573,8 @@ const line_array = [
     "What have I done to be remembered for?",
     "What have I ever done to deserve to be mourned?",
     "Was I really good to people?",
-    "I can't remember stumbling upon any successes on my way here.",
-    "Am I being negative?",
+    "I can't remember stumbling upon any successes on my way here, either.",
+    "Ah, am I being negative?",
     "Of course I am.",
     "But how could I not be?",
     "I wake up every day unsure of whether or not I truly did wake up.",
@@ -647,7 +629,183 @@ const line_array = [
     "It's depressing, but it's just how it is.",
     "Isn't it?",
 
+// Fade out
+
+    "",
+
+// Alarm wakes MC up
+
+    "God, I feel AWFUL.",
+    "Wait, I'm gonna be late-",
+    "Hold on.",
+    "What time is it?",
+    "Hard to tell with the blinds closed.",
+    "Ah.",
+    "Right.",
+    "I guess I took a nap.",
+    "Yeah.",
+    "Wait, is it Sunday or Saturday, again?",
+
+    // MC checks their phone
+
+    "Okay, that makes sense.",
+    "School again?",
+
+    // Walk around house
+
+        // MC checks kitchen
+        "It's clean. Guess there must've been guests.",
+        // MC checks living room
+        "",
+        // MC checks their phone
+    // Walk outside
+
+    // End of day
+
     "Will you help me?",
+];
+
+
+const linefr_array = [
+    "Vous vous êtes déja demandé si quelque chose n'allait pas?",
+    "Comme si vous n'étiez pas vraiment réveillé?",
+    "C'est ce vide qu'on ressent en allant du point A au point B.",
+    "Comme si tout n'était rien.",
+    "Rien qu'un rêve.",
+    "Une sensation qui ne s'en va jamais.",
+    "Elle te suit, partout où tu vas.",
+    "Et elle ne lache pas.",
+    "Des fois, on l'oublie.",
+    "Pas qu'elle soit partie, on s'y habitue seulement.",
+    "De la même façon qu'on s'habitue aux battements de son coeur.",
+    "À notre respiration, nos propres mouvements.",
+    "Mais quand on s'en rend compte, c'est terrifiant.",
+    "C'est un rappel qu'on n'y échappera jamais.",
+    "Que lentement, mais sûrement, chaque partie de nous y tombera.",
+    "Et au fur et à mesure, les choses paraissent devenir de plus en plus étrange.",
+    "Comme si tout s'écroulait autour de toi.",
+    "C'est qu'une question de temps.",
+    "Cet ennui total s'étend.",
+    "Et ne s'arrête jamais.",
+    "Peut-être que je cherche un sens à quelque chose qui n'en a pas.",
+    "Peut-être que je veux croire que tout ça n'est pas pour rien.",
+    "Qu'il y a une raison à tout ça.",
+    "Qu'il y a quelque chose que je dois faire avec ce fardeau.",
+    "Ou peut-être que j'éspère juste ne pas être seul.",
+    "En pensant à tout ça, en entendant mes propres mots...",
+    "Ils semblent plus familiers.",
+    "C'est si normal, maintenant.",
+    "Et je me demande souvent si tout le monde pense pareil.",
+    "Enfin, je pourrais pas savoir.",
+    "C'est pas comme si j'avais demandé à qui que ce soit.",
+    "Comment commencer une conversation sur ce sujet?",
+    "Qu'est-ce que je devrais dire?",
+    "Et puis, à qui demander?",
+    "Personne n'a l'air de sentir ce que je sens.",
+    "Il n'y a pas de points communs à relier.",
+    "À propos de ces rêves, de ces cauchemars, ou même mes sentiments.",
+    "C'est désespérant.",
+    "Ma façon de voir le monde, de vivre, mes pensées, et mes émotions.",
+    "Aucunes ne seront mises à la lumière du jour.",
+    "Personne ne saura jamais ce que c'était d'être moi.",
+    "Je vais continuer de souffrir, et après, je vais mourir.",
+    "Je vais disparaître de ce monde de la même façon que j'y suis entré.",
+    "Sans un seul son, pas même un pleur.",
+    "Et je ne peux pas me plaindre.",
+    "Même si je voudrais tant pouvoir changer les chose, je ne peux pas.",
+    "Est-ce que ça ne serait pas égoïste d'éxiger qu'on se souvienne de nous?",
+    "Qu'est-ce que j'ai fait pour qu'on se souvienne de moi?",
+    "Qu'est-ce que j'ai fait pour mériter d'être pleuré?",
+    "Est-ce que j'ai vraiment été bon envers les autres?",
+    "Je ne me souviens pas avoir rencontré du succès sur mon chemin, non plus.",
+    "Ah, est-ce que je suis pessimiste?",
+    "Évidemment.",
+    "Comment ne pas l'être?",
+    "Je me lève chaque jour sans même la certitude de m'être vraiment levé.",
+    '«Je pense, donc je suis.»',
+    "Bah, je pense beaucoup, ça, c'est sûr.",
+    "Mais est-ce que je suis?",
+    "C'est drôle, mais je n'ai même pas le privilège de me sentir en vie.",
+    "Sérieusement, je me sens plus à l'aise dans un cauchemar.",
+    "Être autour de monstres défigurés est toujours mieux que devoir respirer cet air.",
+    "Mon corps ne me semble plus mien.",
+    "L'a-t-il un jour été?",
+    "Est-ce que c'est normal?",
+    "Encore une fois, je reviens à cette question.",
+    "La réponse est pourtant si simple.",
+    '«Je ne le saurais jamais.»',
+    "Oui, je sais.",
+    "Je sais bien.",
+    "Je sais très bien.",
+    "Je le sais très, trop, tellement bien.",
+    "Alors pourquoi je continue d'y penser?",
+    "Est-ce que j'éspère que la réponse va changer?",
+    "J'en suis au point d'espérer perdre la raison.",
+    "Je ne veux plus faire face à la réalité.",
+    "Ça fait déjà des années que ça dure.",
+    "J'aimerais pouvoir m'accrocher à quelque chose, n'importe quoi.",
+    "La foi, les promesses, ou les jolis mensonges, par exemple.",
+    "Mais je n'y peux rien.",
+    "La foi n'est pas quelque chose qu'on choisit.",
+    "Je ne peux changer ni la réalité, ni moi-même.",
+    "Si seulement quelqu'un pouvais me dire ces deux mots:",
+    '«Je comprends.»',
+    "Je veux qu'on me comprenne, et comprendre en retour.",
+    "Je le veux plus que tout au monde.",
+    "Je veux parler, je veux dire quelque chose, et qu'on le comprenne.",
+    "Je veux trouver quelqu'un comme moi.",
+    "J'en ai besoin.",
+    "Mais ça n'arrivera pas, hein?",
+    "L'espoir ne fait pas bouger les choses par magie.",
+    "Faut toujours travailler pour avoir.",
+    "Oui, travailler pour avoir, bien sûr.",
+    "Mais comment travailler pour avoir ça?",
+    "Je dois me calmer.",
+    "Penser ne sert à rien.",
+    "C'est horrible.",
+    "Comme si j'ouvrais mes yeux seulement pour regarder le soleil.",
+    "Pourquoi faire?",
+    "Je ne peux pas.",
+    "On ne peux pas être quelqu'un d'autre.",
+    "Tu ne seras jamais que toi-même.",
+    "C'est juste comme ça.",
+    "C'est cruel, mais c'est comme ça.",
+    "C'est déprimant, mais c'est comme ça.",
+    "Non?",
+
+// Fade out
+
+    "",
+
+// Alarm wakes MC up
+
+    "Oh là là, ça va vraiment pas.",
+    "Attends, je vais être en retard-",
+    "Ah, mais.",
+    "Il est quelle heure?",
+    "Dur à savoir avec les rideaux fermés.",
+    "Ah.",
+    "C'est vrai.",
+    "J'ai du faire une sieste.",
+    "Ouais.",
+    "Au fait, c'est samedi ou dimanche, déjà?",
+
+    // MC checks their phone
+
+    "Ok, je vois.",
+
+    // Walk around house
+
+        // MC checks kitchen
+        "It's clean. Guess there must've been guests.",
+        // MC checks living room
+        "",
+        // MC checks their phone
+    // Walk outside
+
+    // End of day
+
+    "Will you help me?"
 ];
 
 var current_line;
@@ -693,6 +851,48 @@ function updateLine()
     else
     {
         console.log("updateLine(): line_array.length reached, return");
+        return;
+    }
+}
+
+function updateLinefr()
+{
+    if(current_line == i_line)
+    {
+        return;
+    }
+    else
+    {
+        current_line = i_line;
+    }
+
+    if(current_line == null)
+    {
+        if(line_interval != null)
+        {
+                clearInterval(line_interval);
+                line_interval = null;
+        }
+        textline.innerHTML = "";
+        return;
+    }
+
+    if (current_line <= linefr_array.length)
+    {
+        if(line_interval != null)
+        {
+            clearInterval(line_interval);
+            line_interval = null;
+        }
+
+        textline.innerHTML = "";
+        i_text = 0;
+
+        line_interval = setInterval(lineAddNextLetter, 40, linefr_array[current_line]);
+    }
+    else
+    {
+        console.log("updateLine(): linefr_array.length reached, return");
         return;
     }
 }
